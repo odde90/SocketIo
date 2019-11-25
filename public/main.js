306abb9b725acdb05b763933c0a43d91f2b71f41
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-11-25 21:28:34 +0100

    get request gif

--- public/main.js
+++ public/main.js
@@ -1,25 +1,32 @@
+const baseURLGify = 'http://api.giphy.com/v1/gifs/trending';
+const apiKeyGify = '9sP4qcJGp890dC9oKmNi7GqR5CEeyv3d';
+var socket = io();
+  
 $(function() {
-
-  var socket = io();
-  $("form").submit(function(e) {
-    e.preventDefault(); // prevents page reloading
-    socket.emit(
-      "chat message",
-      $("#m").val()
-    );
-    $("#m").val("");
-    return false;
+  $("form").submit( async function(e) {
+    e.preventDefault(); 
+    var test = $("#m").val()
+    if(test == '/gif'){
+      try{
+        let respons = await gifUrl();
+        let imgUrl = await filterGifs(respons);
+        console.log(imgUrl)
+        socket.emit("chat message", imgUrl);
+      }catch(err){
+        console.error(err)
+      }
+    }else{
+      console.log(false)
+      socket.emit(
+        "chat message",
+        $("#m").val()
+        );
+  
+      $("#m").val("");
+      return false;
+    }
   });
 
-  $("form").submit(function(e) {
-    e.preventDefault(); // prevents page reloading
-    socket.emit(
-      "chat message",
-      $("#m").val()
-    );
-    $("#m").val("");
-    return false;
-  });
   socket.on(
     "chat message",
     function(msg) {
@@ -31,3 +38,32 @@ $(function() {
 
 
 });
+
+
+function gifUrl() {
+  let url = new URL(baseURLGify)
+  url.search = new URLSearchParams({
+    api_key: apiKeyGify
+  })
+  return makeRequest(url)
+}
+
+async function makeRequest(url) {
+    let respons = await fetch(url);
+    if(respons.status != 200){
+
+    }else{
+      return await respons.json();
+    }  
+}
+
+async function filterGifs(gifarray) {
+  var gifList = [];
+
+  gifarray.data.forEach(item => {
+     gifList.push(item.embed_url);
+  });
+
+  var rand = gifList[Math.floor(Math.random() * gifList.length)];
+  return rand;
+}
