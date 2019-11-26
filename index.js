58d3080becce5068c1217f5cc458c312d276f19e
odde90 <43670486+odde90@users.noreply.github.com>
2019-11-26 15:59:15 +0100

    show name of user

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
+const users = {};
+
+io.on("connection", socket => {
+  socket.on("new-user", name => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", message => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: [socket.id]
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+
+  /* socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  }); */
+});
+/* skapa namn här */
+
 app.get("/", function(req, res) {
-  res.sendFile(
-    __dirname + "/index.html"
-  );
+  res.sendFile(__dirname + "/index.html");
 });
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public")); // This will emit the event to all connected socket */
 
+/* socket.on("disconnect", function() {
+  console.log("user disconnected");
+}); */
 
-
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("chat message", function(
-    msg
-  ) {
-    socket.broadcast.emit("hi");
-    console.log("message:" + msg);
-    io.emit("chat message", msg);
-  });
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
-  });
-});
-
-io.emit("some event", {
+/* io.emit("some event", {
   someProperty: "some value",
   otherProperty: "other value"
-}); // This will emit the event to all connected socket
-
-http.listen(3000, function() {
-  console.log("listenning on *:3000");
-});
- 
+}); */ http.listen(
+  3000,
+  function() {
+    console.log("listenning on *:3000");
+  }
+);
--- public/main.js
+++ public/main.js
@@ -1,67 +1,96 @@
-const baseURLGify = 'http://api.giphy.com/v1/gifs/trending';
-const apiKeyGify = '9sP4qcJGp890dC9oKmNi7GqR5CEeyv3d';
+/* import { connect } from "net"; */
+
+const baseURLGify = "http://api.giphy.com/v1/gifs/trending";
+const apiKeyGify = "9sP4qcJGp890dC9oKmNi7GqR5CEeyv3d";
 var socket = io();
-  
+const messageForm = document.getElementById("send-container");
+const messageContainer = document.getElementById("Messages-Container");
+const messageInput = document.getElementById("message-input");
+const handle = document.getElementById("Handle");
+const feedback = document.getElementById("feedback");
+
+const name = prompt("What is your name?");
+appendMessage("You joined");
+socket.emit("new-user", name);
+
+socket.on("chat-message", data => {
+  appendMessage(`${data.name}: ${data.message}`);
+});
+
+socket.on("user-connected", name => {
+  appendMessage(`${name} connected`);
+});
+
+socket.on("user-disconnected", name => {
+  appendMessage(`${name} disconnected`);
+});
+
 $(function() {
-  $("form").submit( async function(e) {
-    e.preventDefault(); 
-    var test = $("#m").val()
-    if(test == '/gif'){
-      try{
+  $("form").submit(async function(e) {
+    e.preventDefault();
+    var test = $("#m").val();
+    if (test == "/gif") {
+      try {
         let respons = await gifUrl();
         let imgUrl = await filterGifs(respons);
-        console.log(imgUrl)
+        console.log(imgUrl);
         socket.emit("chat message", imgUrl);
-      }catch(err){
-        console.error(err)
+      } catch (err) {
+        console.error(err);
       }
-    }else{
-      console.log(false)
-      socket.emit(
-        "chat message",
-        $("#m").val()
-        );
-  
+    } else {
+      console.log(false);
+      socket.emit("chat message", $("#m").val());
+
       $("#m").val("");
       return false;
     }
   });
-
-  socket.on(
-    "chat message",
-    function(msg) {
-      $("#messages").append(
-        $("<li>").text(msg)
-      );
-    }
-  );
-
-
 });
 
+messageForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`you: ${message}`);
+  socket.emit("send-chat-message", message);
+  messageInput.value = "";
+});
+
+function appendMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
+
+/* messageForm.addEventListener("keypress", function() {
+  socket.emit("typing", handle.value);
+});
+
+socket.on("typing", function(data) {
+  feedback.innerHTML = "<p><em>" + data + "is typing...</em></p>";
+}); */
 
 function gifUrl() {
-  let url = new URL(baseURLGify)
+  let url = new URL(baseURLGify);
   url.search = new URLSearchParams({
     api_key: apiKeyGify
-  })
-  return makeRequest(url)
+  });
+  return makeRequest(url);
 }
 
 async function makeRequest(url) {
-    let respons = await fetch(url);
-    if(respons.status != 200){
-
-    }else{
-      return await respons.json();
-    }  
+  let respons = await fetch(url);
+  if (respons.status != 200) {
+  } else {
+    return await respons.json();
+  }
 }
 
 async function filterGifs(gifarray) {
   var gifList = [];
 
   gifarray.data.forEach(item => {
-     gifList.push(item.embed_url);
+    gifList.push(item.embed_url);
   });
 
   var rand = gifList[Math.floor(Math.random() * gifList.length)];
